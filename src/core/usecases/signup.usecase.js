9f2f0b8c0fc45b9a6f81f7f040b74a1614253177
Jefferson Gibin <jefferson@jefferson.gibin>
2023-03-18 09:01:38 -0300

    DPP-12 implemented usecase

---
+++ src/core/usecases/signup.usecase.js
@@ -0,0 +1,46 @@
+const exceptions = [
+  "InvalidPasswordException",
+  "InvalidParameterException",
+  "InvalidParameterType",
+  "UsernameExistsException",
+];
+
+export class SignUpUseCase {
+  #signUpRepository;
+  #userDTO;
+
+  constructor(repository, userDTO) {
+    this.#signUpRepository = repository;
+    this.#userDTO = userDTO;
+  }
+
+  async execute() {
+    try {
+      await this.#signUpRepository.addUser({
+        name: this.#userDTO.name,
+        email: this.#userDTO.email,
+        password: this.#userDTO.password,
+      });
+
+      return {
+        status: "SUCCESS",
+        message: "OK",
+      };
+    } catch (error) {
+      const messageError = error.message;
+      const exceptionCode = error.code;
+
+      if (exceptions.includes(exceptionCode)) {
+        return {
+          status: "ERROR",
+          message: messageError,
+        };
+      }
+
+      return {
+        status: "ERROR",
+        message: "Validation Error",
+      };
+    }
+  }
+}
